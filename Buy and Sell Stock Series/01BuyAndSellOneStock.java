2490d010c735f235bef7fad1042ce8182ddd7cb7
Rajarshi Sengupta <rajarshisengupta50@gmail.com>
2021-08-26 23:38:00 +0530

    Update 01BuyAndSellOneStock.java

--- Buy and Sell Stock Series/01BuyAndSellOneStock.java
+++ Buy and Sell Stock Series/01BuyAndSellOneStock.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        //max profit --> buy the stock on the day when it's price is least and sell it when it is maximum
         int maxProfit = 0, min = 0, max = 0;
         
         for(int i=1;i<prices.length;i++) {
