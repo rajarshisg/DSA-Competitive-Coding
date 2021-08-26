289a2d7b6c202c9de6e26ae25c9f2caac49eb95d
Rajarshi Sengupta <rajarshisengupta50@gmail.com>
2021-08-26 23:35:48 +0530

    Update 03.BuyAndSellWithFees.java

--- Buy and Sell Stock Series/03.BuyAndSellWithFees.java
+++ Buy and Sell Stock Series/03.BuyAndSellWithFees.java
@@ -7,11 +7,11 @@ class Solution {
         */
         
         dp[0][0] = 0;
-        dp[0][1] = -prices[0] - fee; //i need to buy the stock to own it on first day
+        dp[0][1] = -prices[0] - fee; //We need to buy the stock to own it on first day
         
         for(int i=1;i<prices.length;i++) {
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]); //to not have stock on ith day either wi a;ready didn't have it or sold a stock
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i] - fee);
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]); //to not have stock on ith day either we already didn't have it or sold a stock
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i] - fee); //either we already owned the stock or bought one
         }
         
         return dp[prices.length - 1][0];
