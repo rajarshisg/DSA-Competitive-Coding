a8eece5b79896a060e3f5dac4439882660cbcb98
Rajarshi Sengupta <rajarshisengupta50@gmail.com>
2021-08-27 01:53:05 +0530

    Create 05BestTimeToBuyAndSellWith2Transactions.java

---
+++ Buy and Sell Stock Series/05BestTimeToBuyAndSellWith2Transactions.java
@@ -0,0 +1,30 @@
+/* LeetCode 123 --> Best Time to Buy and Sell Stock III */
+class Solution {
+    public int maxProfit(int[] prices) {
+        /* For One Transaction we need to buy the stock on the day it has minimum value and sell on the day it has maximum */
+        /* For Two transactions we can partition at every given day and find the max of one transaction on days before it ans on days after it ans add them up */
+        /* the maximum of all of this will be our answer */
+        
+        int[] leftProfit = new int[prices.length]; // leftProfit[i] --> max profit on ith day staring from 0th day
+        int[] rightProfit = new int[prices.length]; // rightProfit[i] --> max profit on ith day starting from last day
+        
+        int minPrice = prices[0];
+        for(int i=1;i<prices.length;i++) {
+            leftProfit[i] = Math.max(leftProfit[i-1], prices[i] - minPrice);
+            minPrice = Math.min(prices[i], minPrice);
+        }
+        
+        int maxPrice = prices[prices.length - 1];
+        for(int i=prices.length - 2;i>=0;i--) {
+            rightProfit[i] = Math.max(rightProfit[i+1], maxPrice - prices[i]);
+            maxPrice = Math.max(prices[i], maxPrice);
+        }
+        
+        //partitioning
+        int ans = rightProfit[0];
+        for(int i=1;i<prices.length;i++) {
+            ans = Math.max(ans, leftProfit[i-1] + rightProfit[i]);
+        }
+        return ans;
+    }
+}
